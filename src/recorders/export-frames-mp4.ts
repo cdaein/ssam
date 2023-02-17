00865a577f7287e12074cb6adc3f0530ad54994c
user <3522003+cdaein@users.noreply.github.com>
2023-02-16 19:35:55 -0500

    set up mp4 recorder

---
+++ src/recorders/export-frames-mp4.ts
@@ -0,0 +1,130 @@
+import type {
+  SketchStates,
+  SketchSettingsInternal,
+  BaseProps,
+} from "../types/types";
+// import { downloadBlob } from "../helpers";
+
+let videoEncoder: VideoEncoder | null = null;
+let lastKeyframe: number | null = null;
+
+export const setupMp4Record = ({
+  canvas,
+  settings,
+}: {
+  canvas: HTMLCanvasElement;
+  settings: SketchSettingsInternal;
+}) => {
+  // TODO: instead, check for ffmpeg availability (vite plugin should send a warning)
+  // if (!("VideoEncoder" in window)) {
+  //   console.warn("The browser does not support WebCodecs");
+  //   return;
+  // }
+
+  const format = "mp4";
+
+  // muxer = new WebMMuxer({
+  //   target: "buffer",
+  //   video: {
+  //     codec: "V_VP9", // TODO: check for codec support
+  //     width: canvas.width,
+  //     height: canvas.height,
+  //     frameRate: settings.exportFps,
+  //   },
+  // });
+
+  // videoEncoder = new VideoEncoder({
+  //   output: (chunk, meta) => muxer?.addVideoChunk(chunk, meta),
+  //   error: (e) => console.error(`WebMMuxer error: ${e}`),
+  // });
+
+  // videoEncoder.configure({
+  //   codec: "vp09.00.10.08", // TODO: look at other codecs
+  //   width: canvas.width,
+  //   height: canvas.height,
+  //   bitrate: 10_000_000, // REVIEW: 1e7 = 10 Mbps (keep high. needs mp4 convert again)
+  // });
+
+  lastKeyframe = -Infinity;
+
+  canvas.style.outline = `3px solid red`;
+  canvas.style.outlineOffset = `-3px`;
+
+  console.log(`recording (${format}) started`);
+};
+
+export const exportMp4 = async ({
+  canvas,
+  settings,
+  states,
+  props,
+}: {
+  canvas: HTMLCanvasElement;
+  states: SketchStates;
+  settings: SketchSettingsInternal;
+  props: BaseProps;
+}) => {
+  // if (!("VideoEncoder" in window)) {
+  //   return;
+  // }
+
+  if (!states.captureDone) {
+    // record frame
+    encodeVideoFrame({ canvas, settings, states, props });
+  }
+};
+
+export const encodeVideoFrame = ({
+  canvas,
+  settings,
+  states,
+  props,
+}: {
+  canvas: HTMLCanvasElement;
+  settings: SketchSettingsInternal;
+  states: SketchStates;
+  props: BaseProps;
+}) => {
+  // timestamp unit is micro-seconds!!
+  const frame = new VideoFrame(canvas, { timestamp: props.time * 1000 });
+
+  // add video keyframe every 2 seconds (2000ms)
+  const needsKeyframe = props.time - lastKeyframe! >= 2000;
+  if (needsKeyframe) lastKeyframe = props.time;
+
+  videoEncoder?.encode(frame, { keyFrame: needsKeyframe });
+  frame.close();
+
+  // TODO: this should be in settings, states or props
+  const totalFrames = Math.floor(
+    (settings.exportFps * settings.duration) / 1000
+  );
+  console.log(`recording (webm) frame... ${props.frame + 1} of ${totalFrames}`);
+};
+
+export const endMp4Record = async ({
+  canvas,
+  settings,
+}: {
+  canvas: HTMLCanvasElement;
+  settings: SketchSettingsInternal;
+}) => {
+  // if (!("VideoEncoder" in window)) {
+  //   return;
+  // }
+
+  const format = "mp4";
+
+  await videoEncoder?.flush();
+  // const buffer = muxer?.finalize();
+
+  // downloadBlob(new Blob([buffer!], { type: "video/webm" }), settings, format);
+
+  // muxer = null;
+  videoEncoder = null;
+
+  canvas.style.outline = "none";
+  canvas.style.outlineOffset = `0 `;
+
+  console.log(`recording (${format}) complete`);
+};
